db8b02a47a36dc99797542c2bcc3fcd509deb1af
Ana-Marija Drašković <ana.marija.draskovic007@gmail.com>
2025-08-27 14:06:17 +0000

    Clean up password generator JS and fix minor issues

--- public/index.js
+++ public/index.js
@@ -2,34 +2,33 @@ const lettersArray = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O
 const numsArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 const symbolsArray = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
 
-let passwordEl = document.getElementById("password-text");
-let generateBtn = document.getElementById("generate-btn");
-let symbolsExcluded = document.querySelector("#no-symbols-checkbox");
-let numsExcluded = document.querySelector("#no-nums-checkbox");
-let numOfLetters = document.querySelector("#letter-num");
-let copyBtn = document.getElementById("copy-text-btn");
+const passwordEl = document.getElementById("password-text");
+const generateBtn = document.getElementById("generate-btn");
+const symbolsExcluded = document.querySelector("#no-symbols-checkbox");
+const numsExcluded = document.querySelector("#no-nums-checkbox");
+const numOfLetters = document.querySelector("#letter-num");
+const copyBtn = document.getElementById("copy-text-btn");
+const changeThemeBtn = document.getElementById("change-theme-btn");
+const root = document.documentElement;
 
 generateBtn.addEventListener("click", generatePasswords);
 
 function generatePasswords() {
   let array = generateArray();
-  let passwordLength = numOfLetters.value;
-  passwordEl.value = generateRandomPasswords(passwordLength, array);
+  let passwordLength = Number(numOfLetters.value);
+  passwordEl.value = generateRandomPassword(passwordLength, array);
   generateBtn.textContent = "New Password";
   copyBtn.innerHTML = '<i class="fa-regular fa-copy"></i> Copy';
 }
 
 function generateArray() {
   let array = lettersArray;
-
   if (!numsExcluded.checked) array = array.concat(numsArray);
-
   if (!symbolsExcluded.checked) array = array.concat(symbolsArray);
-
   return array;
 }
 
-function generateRandomPasswords(length, array) {
+function generateRandomPassword(length, array) {
   let password = "";
   for (let i = 0; i < length; i++) {
     let randomNum = Math.floor(Math.random() * array.length);
@@ -44,11 +43,12 @@ function copyOnClick() {
   let pwdToCopy = passwordEl.value;
   navigator.clipboard.writeText(pwdToCopy).then(() => {
     copyBtn.textContent = "Copied!";
+    setTimeout(
+      () => (copyBtn.innerHTML = '<i class="fa-regular fa-copy"></i> Copy'),
+      2000,
+    );
   });
 }
-
-const changeThemeBtn = document.getElementById("change-theme-btn");
-const root = document.documentElement;
 
 changeThemeBtn.addEventListener("click", () => {
   if (root.getAttribute("data-theme") === "dark") {
@@ -57,9 +57,5 @@ changeThemeBtn.addEventListener("click", () => {
   } else {
     root.setAttribute("data-theme", "dark");
     localStorage.setItem("theme", "dark");
-  }
-
-  if (localStorage.getItem("theme") === "dark") {
-    root.setAttribute("data-theme", "dark");
   }
 });
